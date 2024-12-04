8401c15ae8afd9c7a70a2213893dbaf98be4972f
konu <test@example.com>
2024-12-04 15:30:13 +0900

    画面作成

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,24 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_board_info(port: &str) -> String {
+    print!("port:{}", port);
+    format!("Port:{},Info:{}", port, "ESP32")
+}
+
+#[tauri::command]
+fn write_firmware(port: &str, baudrate: &str) -> String {
+    print!("port:{}", port);
+    print!("baudrate:{}", baudrate);
+    format!("Port:{},Baudrate:{},Result:{}", port, baudrate, "Success")
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_board_info,
+            write_firmware
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
