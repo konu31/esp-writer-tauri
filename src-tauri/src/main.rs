f9424cc96a8c59d7a7d9651b84ffd6aa0193d832
konu <test@example.com>
2024-12-04 12:30:33 +0900

    ビルド

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Windowsでコンソールが表示される場合は以下の設定にする
+// #![cfg_attr(
+//     all(target_os = "windows"),
+//     windows_subsystem = "windows"
+// )]
 
 fn main() {
     esp_writer_lib::run()
